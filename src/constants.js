257152f35fb0a162d374e081eca8aef86fcc7cf1
Miguel Valdez <mavd22mv@gmail.com>
2022-11-24 14:51:20 -0400

    Initial setup of SuperTokens

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,7 @@
 import * as dotenv from "dotenv";
+import Dashboard from "supertokens-node/recipe/dashboard";
+import Passwordless from "supertokens-node/recipe/passwordless";
+import Session from "supertokens-node/recipe/session";
 
 dotenv.config();
 
@@ -6,4 +9,29 @@ export const ENV = {
   DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
   PORT: Number(process.env.PORT || "8080"),
+  SUPERTOKENS_CONNECTION_URI: process.env.SUPERTOKENS_CONNECTION_URI,
+  SUPERTOKENS_API_KEY: process.env.SUPERTOKENS_API_KEY,
+};
+
+export const SUPERTOKENS_CONFIG = {
+  framework: "express",
+  supertokens: {
+    connectionURI: ENV.SUPERTOKENS_CONNECTION_URI,
+    apiKey: ENV.SUPERTOKENS_API_KEY,
+  },
+  appInfo: {
+    appName: "Marketing data visualization",
+    apiDomain: "http://localhost:8080",
+    websiteDomain: "http://localhost:3000",
+    apiBasePath: "/auth",
+    websiteBasePath: "/auth",
+  },
+  recipeList: [
+    Passwordless.init({
+      flowType: "USER_INPUT_CODE",
+      contactMethod: "EMAIL",
+    }),
+    Session.init(),
+    Dashboard.init({ apiKey: ENV.SUPERTOKENS_API_KEY }),
+  ],
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import consola from "consola";
+import cors from "cors";
 import express from "express";
+import supertokens from "supertokens-node";
+import { middleware } from "supertokens-node/framework/express";
 
-import { ENV } from "@/constants";
+import { ENV, SUPERTOKENS_CONFIG } from "@/constants";
 import { connectDb } from "@/db";
+
+supertokens.init(SUPERTOKENS_CONFIG);
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+    credentials: true,
+  })
+);
+app.use(middleware());
 app.use(express.json());
 
 app.listen(ENV.PORT, async () => {
