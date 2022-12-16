db5977a096a82346d8399e92461c07e470c3f056
Miguel Valdez <mavd22mv@gmail.com>
2022-12-16 10:43:58 -0400

    WIP

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,4 @@
 import * as dotenv from "dotenv";
-import Dashboard from "supertokens-node/recipe/dashboard";
-import Passwordless from "supertokens-node/recipe/passwordless";
-import Session from "supertokens-node/recipe/session";
 
 dotenv.config();
 
@@ -11,27 +8,9 @@ export const ENV = {
   PORT: Number(process.env.PORT || "8080"),
   SUPERTOKENS_CONNECTION_URI: process.env.SUPERTOKENS_CONNECTION_URI,
   SUPERTOKENS_API_KEY: process.env.SUPERTOKENS_API_KEY,
-};
-
-export const SUPERTOKENS_CONFIG = {
-  framework: "express",
-  supertokens: {
-    connectionURI: ENV.SUPERTOKENS_CONNECTION_URI,
-    apiKey: ENV.SUPERTOKENS_API_KEY,
+  REACT_APP_WEBSITE_URL:
+    process.env.REACT_APP_WEBSITE_URL || "http://localhost:3000",
+  get REACT_APP_API_URL() {
+    return process.env.REACT_APP_API_URL || `http://localhost:${this.PORT}`;
   },
-  appInfo: {
-    appName: "Marketing data visualization",
-    apiDomain: "http://localhost:8080",
-    websiteDomain: "http://localhost:3000",
-    apiBasePath: "/auth",
-    websiteBasePath: "/auth",
-  },
-  recipeList: [
-    Passwordless.init({
-      flowType: "USER_INPUT_CODE",
-      contactMethod: "EMAIL",
-    }),
-    Session.init(),
-    Dashboard.init({ apiKey: ENV.SUPERTOKENS_API_KEY }),
-  ],
 };
--- src/index.js
+++ src/index.js
@@ -3,17 +3,41 @@ import cors from "cors";
 import express from "express";
 import supertokens from "supertokens-node";
 import { middleware } from "supertokens-node/framework/express";
+import Dashboard from "supertokens-node/recipe/dashboard";
+import Passwordless from "supertokens-node/recipe/passwordless";
+import Session from "supertokens-node/recipe/session";
 
-import { ENV, SUPERTOKENS_CONFIG } from "@/constants";
+import { ENV } from "@/constants";
 import { connectDb } from "@/db";
 
-supertokens.init(SUPERTOKENS_CONFIG);
+supertokens.init({
+  framework: "express",
+  supertokens: {
+    connectionURI: ENV.SUPERTOKENS_CONNECTION_URI,
+    apiKey: ENV.SUPERTOKENS_API_KEY,
+  },
+  appInfo: {
+    appName: "Marketing data visualization",
+    apiDomain: ENV.REACT_APP_API_URL,
+    websiteDomain: ENV.REACT_APP_WEBSITE_URL,
+    apiBasePath: "/auth",
+    websiteBasePath: "/auth",
+  },
+  recipeList: [
+    Passwordless.init({
+      flowType: "USER_INPUT_CODE",
+      contactMethod: "EMAIL",
+    }),
+    Session.init(),
+    Dashboard.init({ apiKey: ENV.SUPERTOKENS_API_KEY }),
+  ],
+});
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ENV.REACT_APP_WEBSITE_URL,
     allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
     credentials: true,
   })
