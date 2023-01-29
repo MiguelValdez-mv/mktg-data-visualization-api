c06d9023835349add13e6265903f5476894118fd
Miguel Valdez <mavd22mv@gmail.com>
2023-01-29 18:42:26 -0400

    Cascading user deletion

--- src/db/models/User/index.js
+++ src/db/models/User/index.js
@@ -1,6 +1,9 @@
+/* eslint-disable */
 import mongoose from "mongoose";
 
 import { USER_ROLES } from "@/constants";
+
+import { Business } from "../Business";
 
 const userSchema = new mongoose.Schema(
   {
@@ -13,5 +16,31 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("findOneAndUpdate", async function () {
+  const user = await this.model.findOne(this.getFilter());
+  const update = this.getUpdate();
+
+  if (user.role === update.role) {
+    return;
+  }
+
+  if (user.role === USER_ROLES.OWNER) {
+    await Business.deleteMany({ ownerId: user._id });
+    return;
+  }
+
+  if (user.role === USER_ROLES.EMPLOYEE) {
+    await Business.updateMany({}, { $pull: { employeeIds: user._id } });
+  }
+});
+
+userSchema.pre("deleteMany", async function () {
+  const users = await this.model.find(this.getFilter());
+  const ids = users.map(({ _id }) => _id);
+
+  await Business.deleteMany({ ownerId: { $in: ids } });
+  await Business.updateMany({}, { $pullAll: { employeeIds: ids } });
+});
 
 export const User = mongoose.model("User", userSchema);
