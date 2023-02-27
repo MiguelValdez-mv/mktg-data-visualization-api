21b5a4ff83e26e45b069349e43f74e67c5d17ffb
Miguel Valdez <mavd22mv@gmail.com>
2023-02-27 12:03:41 -0400

    Widget cascade elimination

--- src/db/models/Connection/index.js
+++ src/db/models/Connection/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 import { CONNECTION_TYPES } from "@/constants";
+import { Widget } from "@/db/models/Widget";
 
 const connectionSchema = new mongoose.Schema(
   {
@@ -18,5 +19,13 @@ const connectionSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+connectionSchema.pre("deleteMany", async function () {
+  const connectionIds = (await this.model.find(this.getFilter())).map(
+    (connection) => connection._id
+  );
+
+  await Widget.deleteMany({ "selector.connectionId": { $in: connectionIds } });
+});
 
 export const Connection = mongoose.model("Connection", connectionSchema);
--- src/db/models/Panel/index.js
+++ src/db/models/Panel/index.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+import { Widget } from "@/db/models/Widget";
+
 const panelSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
@@ -14,5 +16,19 @@ const panelSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+panelSchema.pre("deleteMany", async function () {
+  const panelIds = (await this.model.findOne(this.getFilter())).map(
+    (panel) => panel._id
+  );
+
+  await Widget.deleteMany({ panelId: { $in: panelIds } });
+});
+
+panelSchema.pre("deleteOne", async function () {
+  const panel = await this.model.findOne(this.getFilter());
+
+  await Widget.deleteMany({ panelId: panel._id });
+});
 
 export const Panel = mongoose.model("Panel", panelSchema);
